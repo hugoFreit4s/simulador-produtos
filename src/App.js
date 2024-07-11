34726c27459a4c2d76ddddb30820682e9c73490d
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-07-11 13:46:24 -0300

    Movendo a tela de boletos para outro diretorio

--- src/App.js
+++ src/App.js
@@ -1,50 +1,11 @@
 import './App.css';
-import InputCurrency from './components/InputCurrency';
-import React from 'react';
 
 function App() {
-
-  const [saldoMedio, setSaldoMedio] = React.useState('');
-  const [percentageSaldoMedio, setpercentageSaldoMedio] = React.useState('');
-
-  const handleSMchange = event => {
-    localStorage.setItem('saldoMedioLS', event.target.value);
-    setSaldoMedio(event.target.value);
-  };
-
-  const handlePercentageSMchange = event => {
-    localStorage.setItem('percentageSaldoMedioLS', event.target.value);
-    setpercentageSaldoMedio(event.target.value);
-  }
-
   return (
-    <div className="App">
-      <header>
-        <img className="logo" alt='' src="logo.png" />
-      </header>
-      <div className="main">
-        <div className="centralizacao-financeira">
-          <h1 className="label" style={{ color: 'white' }}>(1) Centralização Financeira</h1>
-
-          <InputCurrency
-            value={saldoMedio}
-            onChange={handleSMchange}
-            placeholder={'Insira aqui!'}
-            title={'(A) Saldo Médio'}
-          />
-          <InputCurrency
-            value={percentageSaldoMedio}
-            onChange={handlePercentageSMchange}
-            placeholder={'Insira aqui!'}
-            title={'(B) % Sobre Saldo Médio'}
-          />
-        </div>
-      </div>
-      <footer>
-
-      </footer>
+    <div className='App'>
+      <button>Boletos</button>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/pages/Boletos.js
@@ -0,0 +1,66 @@
+import InputCurrency from '../components/InputCurrency';
+import React from 'react';
+
+function Boletos() {
+
+    const [saldoMedio, setSaldoMedio] = React.useState('');
+    const [percentageSaldoMedio, setpercentageSaldoMedio] = React.useState('');
+    const [taxaCDI, setTaxaCDI] = React.useState('');
+    const [percentageCetralizacao, setPercentageCetralizacao] = React.useState('');
+    const [diasFloat, setDiasFloat] = React.useState('');
+
+    const handleSMchange = event => {
+        localStorage.setItem('saldoMedioLS', event.target.value);
+        setSaldoMedio(event.target.value);
+    };
+
+    const handlePercentageSMchange = event => {
+        localStorage.setItem('percentageSaldoMedioLS', event.target.value);
+        setpercentageSaldoMedio(event.target.value);
+    }
+
+    const handleTaxaCDI = event => {
+        localStorage.setItem('taxaCDI', event.target.value);
+        setTaxaCDI(event.target.value);
+    }
+
+    const handlePercentageCentralizacao = event => {
+        localStorage.setItem('percentageCetralizacao', event.target.value);
+        setPercentageCetralizacao(event.target.value);
+    }
+
+    const handleDiasFloatChange = event => {
+        localStorage.setItem('diasFloat', event.target.value);
+        setDiasFloat(event.target.value);
+    }
+    return (
+        <div className="App">
+            <header>
+                <img className="logo" alt='' src="logo.png" />
+            </header>
+            <div className="main">
+                <div className="centralizacao-financeira-1">
+                    <h1 className="label" style={{ color: 'white' }}>(1) Centralização Financeira</h1>
+
+                    <InputCurrency value={saldoMedio} onChange={handleSMchange} placeholder={'Insira aqui!'} title={'(A) Saldo Médio'} />
+                    <InputCurrency value={percentageSaldoMedio} onChange={handlePercentageSMchange} placeholder={'Insira aqui!'} title={'(B) % Sobre Saldo Médio'} />
+                    <InputCurrency value={taxaCDI} onChange={handleTaxaCDI} placeholder={'Insira aqui!'} title={'(C) Taxa CDI (a.m)'} />
+                    <InputCurrency value={percentageCetralizacao} onChange={handlePercentageCentralizacao} placeholder={'Insira aqui!'} title={'(D) % Centralização'} />
+
+                    <p style={{ color: 'white' }}>(=) Receita Estimada</p>
+                </div>
+
+                <div className="centralizacao-financeira-2">
+                    <h1 className="label" style={{ color: 'white' }}>(2) Centralização Financeira</h1>
+
+                    <InputCurrency value={diasFloat} onChange={handleDiasFloatChange} placeholder={'Insira aqui!'} title={'(A) Dias de Float'} />
+                </div>
+            </div>
+            <footer>
+
+            </footer>
+        </div>
+    )
+}
+
+export default Boletos;
