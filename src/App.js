64259fe96c784e58a0499af7f8c033d83261ece9
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-07-11 10:45:35 -0300

    Primeiro commit. Componente INPUT adicionado, armazenamento dos dados em Local Storage funcionando

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import './App.css';
 import { Link } from 'react-router-dom';
 
 function App() {
+
+  const [saldoMedio, setSaldoMedio] = React.useState('');
+  const [percentageSaldoMedio, setpercentageSaldoMedio] = React.useState('');
+
+  const handleSMchange = event => {
+    localStorage.setItem('saldoMedioLS', event.target.value);
+    setSaldoMedio(event.target.value);
+  };
+
+  const handlePercentageSMchange = event => {
+    localStorage.setItem('percentageSaldoMedioLS', event.target.value);
+    setpercentageSaldoMedio(event.target.value);
+  }
+
   return (
     <div className="App">
       <Link to="/boletos">
