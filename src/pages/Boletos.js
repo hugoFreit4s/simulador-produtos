78d058a66f3f0cc8b436c6aa92a885247113fb2a
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-07-12 11:04:08 -0300

    Checkbox dinamica adicionada

--- src/pages/Boletos.js
+++ src/pages/Boletos.js
@@ -3,6 +3,7 @@ import React from 'react';
 import InputCurrency from '../components/InputCurrency';
 import { Link } from 'react-router-dom';
 import './Boletos.css';
+import { Checkbox } from '@mui/joy';
 
 function Boletos() {
     const [saldoMedio, setSaldoMedio] = React.useState('');
@@ -12,6 +13,11 @@ function Boletos() {
     const [diasFloat, setDiasFloat] = React.useState('');
     const [CDIauferido1Dia, setCDIauferido1Dia] = React.useState('');
     const [percentageCentralizacao2, setpercentageCentralizacao2] = React.useState('');
+
+    const [isChecked, setIsChecked] = React.useState('');
+    const [isChecked2, setIsChecked2] = React.useState('');
+
+
 
     const handleSMchange = event => {
         localStorage.setItem('saldoMedioLS', event.target.value);
@@ -48,6 +54,19 @@ function Boletos() {
         setpercentageCentralizacao2(e.target.value);
     }
 
+    const handleCheckbox1 = (event) => {
+        setIsChecked(event.target.checked);
+        if (event.target.checked) {
+            setIsChecked2(false);
+        }
+    };
+
+    const handleCheckbox2 = (event) => {
+        setIsChecked2(event.target.checked);
+        if (event.target.checked) {
+            setIsChecked(false);
+        }
+    };
     return (
         <div className="Boletos">
             <Link to="/">
@@ -74,6 +93,23 @@ function Boletos() {
                     <InputCurrency value={diasFloat} onChange={handleDiasFloat} placeholder={'Insira aqui!'} title={'(A) Dias de Float'} />
                     <InputCurrency value={CDIauferido1Dia} onChange={handleCDIauferido1Dia} placeholder={'Insira aqui!'} title={'(B) Taxa CDI auferida por 1 dia'} />
                     <InputCurrency value={percentageCentralizacao2} onChange={handlePercentageCentralizacao2} placeholder={'Insira aqui!'} title={'(C) % Centralização'} />
+
+                    <p className="common-text" style={{ color: 'white' }}>(=) Receita estimada:</p>
+                    <p className="common-text" style={{ color: 'white' }}>(=) Receita total (1) + (2):</p>
+                    <p className="common-text" style={{ color: 'white' }}>(=) Quantidade boletos liquidados:</p>
+                    <p className="common-text" style={{ color: 'white' }}>(=) Receita gerada por boleto:</p>
+
+                    <hr />
+
+                    <div className="inputs">
+                        <div id="input-1">
+                            <p className="common-text">Protesto:</p>
+                            <div style={{ display: 'flex', gap: '40px' }}>
+                                <Checkbox checked={isChecked} color="primary" size="md" variant="solid" label="SIM" onChange={handleCheckbox1} />
+                                <Checkbox checked={isChecked2} color="primary" size="md" variant="solid" label="NÃO" onChange={handleCheckbox2} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <footer>
