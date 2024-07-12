7d44a578778b0ec747d0d21891bdf97e05c48aca
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-07-12 10:02:50 -0300

    Novos estilos

--- src/pages/Boletos.js
+++ src/pages/Boletos.js
@@ -10,6 +10,8 @@ function Boletos() {
     const [taxaCDI, setTaxaCDI] = React.useState('');
     const [percentageCetralizacao, setPercentageCetralizacao] = React.useState('');
     const [diasFloat, setDiasFloat] = React.useState('');
+    const [CDIauferido1Dia, setCDIauferido1Dia] = React.useState('');
+    const [percentageCentralizacao2, setpercentageCentralizacao2] = React.useState('');
 
     const handleSMchange = event => {
         localStorage.setItem('saldoMedioLS', event.target.value);
@@ -31,15 +33,25 @@ function Boletos() {
         setPercentageCetralizacao(event.target.value);
     };
 
-    const handleDiasFloatChange = event => {
+    const handleDiasFloat = event => {
         localStorage.setItem('diasFloat', event.target.value);
         setDiasFloat(event.target.value);
     };
 
+    const handleCDIauferido1Dia = e => {
+        localStorage.setItem('CDIAuferido1Dia', e.target.value);
+        setCDIauferido1Dia(e.target.value);
+    }
+
+    const handlePercentageCentralizacao2 = e => {
+        localStorage.setItem('percentageCentralizacao2', e.target.value);
+        setpercentageCentralizacao2(e.target.value);
+    }
+
     return (
         <div className="Boletos">
             <Link to="/">
-                <i className="fa fa-arrow-circle-left" style={{ fontSize: '40px', cursor: 'pointer', color: 'white', marginLeft: '20px' }}></i>
+                <i className="fa fa-arrow-circle-left" style={{ fontSize: '40px', cursor: 'pointer', color: 'white', marginLeft: '20px', marginTop: '5px' }}></i>
             </Link>
             <div className="main">
                 <header>
@@ -59,7 +71,9 @@ function Boletos() {
                 <div className="centralizacao-financeira-2">
                     <h1 className="label" style={{ color: 'white' }}>(2) Centralização Financeira</h1>
 
-                    <InputCurrency value={diasFloat} onChange={handleDiasFloatChange} placeholder={'Insira aqui!'} title={'(A) Dias de Float'} />
+                    <InputCurrency value={diasFloat} onChange={handleDiasFloat} placeholder={'Insira aqui!'} title={'(A) Dias de Float'} />
+                    <InputCurrency value={CDIauferido1Dia} onChange={handleCDIauferido1Dia} placeholder={'Insira aqui!'} title={'(B) Taxa CDI auferida por 1 dia'} />
+                    <InputCurrency value={percentageCentralizacao2} onChange={handlePercentageCentralizacao2} placeholder={'Insira aqui!'} title={'(C) % Centralização'} />
                 </div>
             </div>
             <footer>
