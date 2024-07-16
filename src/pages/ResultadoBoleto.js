bb36fe10a8d1573a7c1075eda9bdd7fc40124c00
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-07-16 13:58:35 -0300

    Mudança no layout da tela de resultado de boletos

--- src/pages/ResultadoBoleto.js
+++ src/pages/ResultadoBoleto.js
@@ -1,54 +1,52 @@
 import { Link } from 'react-router-dom';
 import './ResultadoBoletos.css';
+import HomeIcon from '@mui/icons-material/Home';
 
 function ResultadoBoleto() {
     return (
         <div className="ResultadoBoletos">
-            <Link to="/boletos">
+            <Link id="navigation" to="/boletos">
                 <i className="fa fa-arrow-circle-left" style={{ fontSize: '40px', cursor: 'pointer', color: 'white', marginLeft: '20px', marginTop: '5px' }}></i>
+            </Link>
+            <Link id="navigation" to="/">
+                <HomeIcon />
             </Link>
             <main>
                 <div className="custos-por-emissao">
                     <h1>Custos por Emissão</h1>
                     <h2>(1) Custos Compensação</h2>
-                    <p>Linhas de custos</p>
                     <div className="cabecalho">
-                        <h2>Nome</h2>
-                        <div id="subcabecalho">
-                            <h2>Custo Unitário</h2>
-                            <h2>Custo Total</h2>
-                        </div>
+                        <h3>Linhas de custos</h3>
+                        <h3>Linhas de custos</h3>
+                        <h3>Linhas de custos</h3>
                     </div>
-                    <hr id="hr-menor" />
-                    <div className="dados">
-                        <div className="nome">
-                            <p>(A) Rede Bancária - X Boletos</p>
-                            <p>(B) Rede Sicoob - X Boletos;</p>
-                            <p>(C) Liquidação Própria Coop. - X Boletos;</p>
-                            <p>(D) Correspondente Bancário - X Boletos;</p>
-                            <p>(E) Processamento - X Boletos;</p>
-                            <p>(F) Tarifa Nova Cobrança - X Boletos;</p>
-                        </div>
-                        <div className="valores-unitarios">
-                            <p>R$0.00,00</p>
-                            <p>R$0.00,00</p>
-                            <p>R$0.00,00</p>
-                            <p>R$0.00,00</p>
-                            <p>R$0.00,00</p>
-                            <p>R$0.00,00</p>
-                        </div>
-                        <div className="valores-totais">
-                            <p>R$0.00,00</p>
-                            <p>R$0.00,00</p>
-                            <p>R$0.00,00</p>
-                            <p>R$0.00,00</p>
-                            <p>R$0.00,00</p>
-                            <p>R$0.00,00</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="ponto-de-equilibrio">
+                    <hr />
 
+                    <div className="info-custos">
+                        <p className="name">(A) Rede Bancária - X Boletos</p>
+                        <p>R$0,XX</p>
+                        <p>R$XXX,XX</p>
+                    </div>
+                    <div className="info-custos">
+                        <p className="name">(B) Rede Sicoob - X Boletos</p>
+                        <p>R$0,XX</p>
+                        <p>R$XXX,XX</p>
+                    </div>
+                    <div className="info-custos">
+                        <p className="name">(C) Liquidação Própria Coop. - X Boletos</p>
+                        <p>R$0,XX</p>
+                        <p>R$XXX,XX</p>
+                    </div>
+                    <div className="info-custos">
+                        <p className="name">(D) Correspondente Bancário - X Boletos</p>
+                        <p>R$0,XX</p>
+                        <p>R$XXX,XX</p>
+                    </div>
+                    <div className="info-custos">
+                        <p className="name">(E) Tarifa Nova Cobrança - X Boletos</p>
+                        <p>R$0,XX</p>
+                        <p>R$XXX,XX</p>
+                    </div>
                 </div>
             </main>
         </div>
