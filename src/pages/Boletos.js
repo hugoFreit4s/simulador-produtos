4e62499918093cfdc1bca9bab6ba15e5051343fd
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-07-17 12:43:06 -0300

    Mudando os dados que são resultados para ResultadoBoleto.js e inserido os dados que serão coletados em Boletos.js

--- src/pages/Boletos.js
+++ src/pages/Boletos.js
@@ -1,34 +1,18 @@
 import './Boletos.css';
-import InputCurrency from '../components/InputCurrency';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { Checkbox } from '@mui/joy';
 import Button from '@mui/joy/Button';
 import CheckIcon from '@mui/icons-material/Check';
 import { ArrowForwardIos } from '@mui/icons-material';
+import InputCurrency from '../components/InputCurrency';
+
+import DisableNumberScroll from '../DisableNumberScroll';
 
 function Boletos() {
+    DisableNumberScroll();
     const [formData, setFormData] = React.useState({
         saldoMedio: '',
-        percentageSaldoMedio: '',
-        taxaCDI: '',
-        percentageCetralizacao: '',
-        diasFloat: '',
-        CDIauferido1Dia: '',
-        percentageCentralizacao2: '',
-        isCheckedProtesto1: false,
-        isCheckedProtesto2: false,
-        diasProtesto: '',
-        tarifaProtesto: '',
-        isCheckedEmissao2Via1: false,
-        isCheckedEmissao2Via2: false,
-        tarifaProrrogacaoTVencido: '',
-        tarifaManutencaoTVencido: '',
-        isCheckedBancoCorrespondente1: false,
-        isCheckedBancoCorrespondente2: false,
-        baixaAutomaticaTVendidos: '',
-        tarifaEmissaoSegundaVia: '',
     });
 
     const handleInputChange = (field, value) => {
@@ -39,95 +23,26 @@ function Boletos() {
         }));
     };
 
-    const handleCheckboxChange = (field1, field2, value) => {
-        setFormData((prevData) => ({
-            ...prevData,
-            [field1]: value,
-            [field2]: value ? false : prevData[field2],
-        }));
-    };
-
     return (
         <div className="Boletos">
             <Link to="/">
                 <i className="fa fa-arrow-circle-left" style={{ fontSize: '40px', cursor: 'pointer', color: 'white', marginLeft: '20px', marginTop: '5px' }}></i>
             </Link>
             <div className="main">
-                <header>
-                    <img className="logo" alt='' src="logo.png" />
-                </header>
-                <div className="centralizacao-financeira">
-                    <h1 className="label" style={{ color: 'white' }}>(1) Centralização Financeira</h1>
-
-                    <InputCurrency className="input" value={formData.saldoMedio} onChange={(e) => handleInputChange('saldoMedio', e.target.value)} placeholder={'Insira aqui!'} title={'(A) Saldo Médio'} />
-                    <InputCurrency className="input" value={formData.percentageSaldoMedio} onChange={(e) => handleInputChange('percentageSaldoMedio', e.target.value)} placeholder={'Insira aqui!'} title={'(B) % Sobre Saldo Médio'} />
-                    <InputCurrency className="input" value={formData.taxaCDI} onChange={(e) => handleInputChange('taxaCDI', e.target.value)} placeholder={'Insira aqui!'} title={'(C) Taxa CDI (a.m)'} />
-                    <InputCurrency className="input" value={formData.percentageCetralizacao} onChange={(e) => handleInputChange('percentageCetralizacao', e.target.value)} placeholder={'Insira aqui!'} title={'(D) % Centralização'} />
-                    <div className="text-box">
-                        <p style={{ color: 'white' }}>(=) Receita Estimada</p>
-                    </div>
-                </div>
-
-                <div className="centralizacao-financeira">
-                    <h1 className="label" style={{ color: 'white' }}>(2) Centralização Financeira</h1>
-
-                    <InputCurrency className="input" value={formData.diasFloat} onChange={(e) => handleInputChange('diasFloat', e.target.value)} placeholder={'Insira aqui!'} title={'(A) Dias de Float'} />
-                    <InputCurrency className="input" value={formData.CDIauferido1Dia} onChange={(e) => handleInputChange('CDIauferido1Dia', e.target.value)} placeholder={'Insira aqui!'} title={'(B) Taxa CDI auferida por 1 dia'} />
-                    <InputCurrency className="input" value={formData.percentageCentralizacao2} onChange={(e) => handleInputChange('percentageCentralizacao2', e.target.value)} placeholder={'Insira aqui!'} title={'(C) % Centralização'} />
-
-                    <div className="text-box">
-                        <p className="common-text" style={{ color: 'white' }}>(=) Receita estimada:</p>
-                        <p className="common-text" style={{ color: 'white' }}>(=) Receita total (1) + (2):</p>
-                        <p className="common-text" style={{ color: 'white' }}>(=) Quantidade boletos liquidados:</p>
-                        <p className="common-text" style={{ color: 'white' }}>(=) Receita gerada por boleto:</p>
-                    </div>
-
-                    <hr style={{ border: '3px solid white' }} />
-
-                    <div className="inputs">
-                        <div className="input-checkbox">
-                            <p className="common-text">Protesto:</p>
-                            <div style={{ display: 'flex', gap: '40px' }}>
-                                <Checkbox checked={formData.isCheckedProtesto1} color="primary" size="sm" variant="outlined" label="SIM" onChange={(e) => handleCheckboxChange('isCheckedProtesto1', 'isCheckedProtesto2', e.target.checked)} />
-                                <Checkbox checked={formData.isCheckedProtesto2} color="primary" size="sm" variant="outlined" label="NÃO" onChange={(e) => handleCheckboxChange('isCheckedProtesto2', 'isCheckedProtesto1', e.target.checked)} />
-                            </div>
-                        </div>
-                        <div className="input">
-                            <InputCurrency className="input" value={formData.diasProtesto} onChange={(e) => handleInputChange('diasProtesto', e.target.value)} placeholder={'Insira aqui!'} title={'Dias de Protesto'} />
-                            <InputCurrency className="input" value={formData.tarifaProtesto} onChange={(e) => handleInputChange('tarifaProtesto', e.target.value)} placeholder={'Insira aqui!'} title={'Tarifa Protesto'} />
-                        </div>
-                        <div className="input-checkbox">
-                            <p className="common-text">Emissão 2ª Via:</p>
-                            <div style={{ display: 'flex', gap: '40px' }}>
-                                <Checkbox checked={formData.isCheckedEmissao2Via1} color="primary" size="sm" variant="outlined" label="SIM" onChange={(e) => handleCheckboxChange('isCheckedEmissao2Via1', 'isCheckedEmissao2Via2', e.target.checked)} />
-                                <Checkbox checked={formData.isCheckedEmissao2Via2} color="primary" size="sm" variant="outlined" label="NÃO" onChange={(e) => handleCheckboxChange('isCheckedEmissao2Via2', 'isCheckedEmissao2Via1', e.target.checked)} />
-                            </div>
-                        </div>
-                        <div className="input">
-                            <InputCurrency className="input" value={formData.tarifaProrrogacaoTVencido} onChange={(e) => handleInputChange('tarifaProrrogacaoTVencido', e.target.value)} placeholder={'Insira aqui!'} title={'Tarifa Prorrogação de Título Vencido'} />
-                            <InputCurrency className="input" value={formData.tarifaManutencaoTVencido} onChange={(e) => handleInputChange('tarifaManutencaoTVencido', e.target.value)} placeholder={'Insira aqui!'} title={'Tarifa Manutenção de Título Vencido'} />
-                        </div>
-                        <div className="input-checkbox">
-                            <p className="common-text">Banco Correspondente:</p>
-                            <div style={{ display: 'flex', gap: '40px' }}>
-                                <Checkbox checked={formData.isCheckedBancoCorrespondente1} color="primary" size="sm" variant="outlined" label="SIM" onChange={(e) => handleCheckboxChange('isCheckedBancoCorrespondente1', 'isCheckedBancoCorrespondente2', e.target.checked)} />
-                                <Checkbox checked={formData.isCheckedBancoCorrespondente2} color="primary" size="sm" variant="outlined" label="NÃO" onChange={(e) => handleCheckboxChange('isCheckedBancoCorrespondente2', 'isCheckedBancoCorrespondente1', e.target.checked)} />
-                            </div>
-                        </div>
-                        <div className="input">
-                            <InputCurrency className="input" value={formData.baixaAutomaticaTVendidos} onChange={(e) => handleInputChange('baixaAutomaticaTVendidos', e.target.value)} placeholder={'Insira aqui!'} title={'Baixa Automatica Titulos Vendidos (Dias)'} />
-                            <InputCurrency className="input" value={formData.tarifaEmissaoSegundaVia} onChange={(e) => handleInputChange('tarifaEmissaoSegundaVia', e.target.value)} placeholder={'Insira aqui!'} title={'Tarifa Emissão Segunda Via'} />
-                        </div>
-                    </div>
-                </div>
-                <div className="buttons">
-                    <Link to="/resultado-boleto">
-                        <Button color="success" startDecorator={<CheckIcon />}>Simular apenas boletos</Button>
-                    </Link>
-                    <Link to="/lista-produtos">
-                        <Button endDecorator={<ArrowForwardIos />}>Simular mais produtos</Button>
-                    </Link>
-                </div>
+                <InputCurrency className="input" value={formData.qtdBoletos} onChange={(e) => handleInputChange('qtdBoletos', e.target.value)} placeholder={'Insira aqui!'} title={'(A) Saldo Médio'} />
+                <InputCurrency className="input" value={formData.percentageTEmitidos} onChange={(e) => handleInputChange('percentageTEmitidos', e.target.value)} placeholder={'Insira aqui!'} title={'(A) Saldo Médio'} />
+                <InputCurrency className="input" value={formData.percentageTBaixadosCedente} onChange={(e) => handleInputChange('percentageTBaixadosCedente', e.target.value)} placeholder={'Insira aqui!'} title={'(A) Saldo Médio'} />
+                <InputCurrency className="input" value={formData.percentageTBaixadosDecurso} onChange={(e) => handleInputChange('percentageTBaixadosDecurso', e.target.value)} placeholder={'Insira aqui!'} title={'(A) Saldo Médio'} />
+                <InputCurrency className="input" value={formData.diasFloat} onChange={(e) => handleInputChange('diasFloat', e.target.value)} placeholder={'Insira aqui!'} title={'(A) Saldo Médio'} />
+                <InputCurrency className="input" value={formData.ticketMedio} onChange={(e) => handleInputChange('ticketMedio', e.target.value)} placeholder={'Insira aqui!'} title={'(A) Saldo Médio'} />
+            </div>
+            <div className="buttons">
+                <Link to="/resultado-boleto">
+                    <Button color="success" startDecorator={<CheckIcon />}>Simular apenas boletos</Button>
+                </Link>
+                <Link to="/lista-produtos">
+                    <Button endDecorator={<ArrowForwardIos />}>Simular mais produtos</Button>
+                </Link>
             </div>
         </div>
     );
