66091952dc46f51ac6b62a563b1ef7cdb6feb55c
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-07-17 13:52:30 -0300

    Adicionando novos elementos na página de boletos

--- src/pages/Boletos.js
+++ src/pages/Boletos.js
@@ -31,12 +31,28 @@ function Boletos() {
                 <i className="fa fa-arrow-circle-left" style={{ fontSize: '40px', cursor: 'pointer', color: 'white', marginLeft: '20px', marginTop: '5px' }}></i>
             </Link>
             <div className="main">
-                <DisplayInfo title={'(I) % da quantidade em rede bancária'} data={80} />
-                <InputCurrency className="input" value={formData.qtdBoletos} onChange={(e) => handleInputChange('qtdBoletos', e.target.value)} placeholder={'Insira aqui!'} title={'A.1) Quantidade de boletos emitidos mensalmente'} />
-                <InputCurrency className="input" value={formData.percentageTLiquidados} onChange={(e) => handleInputChange('percentageTLiquidados', e.target.value)} placeholder={'Insira aqui!'} title={'A.2) % dos títulos que são liquidados'} />
-                <InputCurrency className="input" value={formData.percentageTBaixadosCedente} onChange={(e) => handleInputChange('percentageTBaixadosCedente', e.target.value)} placeholder={'Insira aqui!'} title={'(I) % dos títulos baixados pedido Cedente'} />
-                <InputCurrency className="input" value={formData.percentageTBaixadosDecurso} onChange={(e) => handleInputChange('percentageTBaixadosDecurso', e.target.value)} placeholder={'Insira aqui!'} title={'(II) % dos títulos baixados por decurso de prazo'} />
-                <InputCurrency className="input" value={formData.diasFloat} onChange={(e) => handleInputChange('diasFloat', e.target.value)} placeholder={'Insira aqui!'} title={'Dias de float'} />
+                <div>
+                    <InputCurrency className="input" value={formData.qtdBoletos} onChange={(e) => handleInputChange('qtdBoletos', e.target.value)} placeholder={'Insira aqui!'} title={'A.1) Quantidade de boletos emitidos mensalmente'} />
+                </div>
+                <div>
+                    <InputCurrency className="input" value={formData.percentageTLiquidados} onChange={(e) => handleInputChange('percentageTLiquidados', e.target.value)} placeholder={'Insira aqui!'} title={'A.2) % dos títulos que são liquidados'} />
+                    <DisplayInfo title={'(I) % da quantidade em rede bancária'} data={43} />
+                    <DisplayInfo title={'(II) % da quantidade em rede Sicoob'} data={7} />
+                    <DisplayInfo title={'(III) % da quantidade em liquidação própria cooperativa'} data={7} />
+                    <DisplayInfo title={'(IV) % da quantidade em liquidação correspondente bancária'} data={43} />
+                </div>
+                <div>
+                    <DisplayInfo title={'A.3) % dos titulos emitidos que são baixados'} data={20} />
+                    <InputCurrency className="input" value={formData.qtdBoletos} onChange={(e) => handleInputChange('qtdBoletos', e.target.value)} placeholder={'Insira aqui!'} title={'(I) % dos títulos baixados pedido cedente'} />
+                    <InputCurrency className="input" value={formData.percentageTBaixadosDecurso} onChange={(e) => handleInputChange('percentageTBaixadosDecurso', e.target.value)} placeholder={'Insira aqui!'} title={'(II) % dos títulos baixados por decurso de prazo'} />
+                </div>
+                <div>
+                    <DisplayInfo title={'A.4) % Direcionadores para precificação do Funding'} data={100} />
+                    <DisplayInfo title={'(I) % Centralização Financeira'} data={100} />
+                    <DisplayInfo title={'(II) % Saldo médio'} data={0} />
+                    <DisplayInfo title={'(III) Taxa (%) CDI no ano (a.a)'} data={13.75} />
+                    <InputCurrency className="input" value={formData.diasFloat} onChange={(e) => handleInputChange('diasFloat', e.target.value)} placeholder={'Insira aqui!'} title={'Dias de float'} />
+                </div>
                 <InputCurrency className="input" value={formData.ticketMedio} onChange={(e) => handleInputChange('ticketMedio', e.target.value)} placeholder={'Insira aqui!'} title={'Ticket médio da carteira'} />
             </div>
             <div className="buttons">
