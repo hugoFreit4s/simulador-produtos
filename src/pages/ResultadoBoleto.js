70042f49035c21d260cfaacf7419d10ab47669af
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-07-17 10:07:03 -0300

    Inseridas tabelas de custos por emissão e ponto de equilíbrio na tela de resultado de boletos

--- src/pages/ResultadoBoleto.js
+++ src/pages/ResultadoBoleto.js
@@ -1,8 +1,23 @@
 import { Link } from 'react-router-dom';
+import React from 'react';
 import './ResultadoBoletos.css';
 import HomeIcon from '@mui/icons-material/Home';
+import { Input } from "@mui/joy";
+import DisableNumberScroll from "../DisableNumberScroll";
 
 function ResultadoBoleto() {
+    DisableNumberScroll();
+    const [formData, setFormData] = React.useState({
+        saldoMedio: '',
+    });
+    const handleInputChange = (field, value) => {
+        localStorage.setItem(field, value);
+        setFormData((prevData) => ({
+            ...prevData,
+            [field]: value,
+        }));
+    };
+
     return (
         <div className="ResultadoBoletos">
             <Link id="navigation" to="/boletos">
@@ -12,71 +27,98 @@ function ResultadoBoleto() {
                 <HomeIcon />
             </Link>
             <main>
-                <div className="custos-por-emissao">
-                    <h1>Custos por Emissão</h1>
-                    <h2>(1) Custos Compensação</h2>
-                    <div className="cabecalho">
-                        <h3>Linhas de custos</h3>
-                        <h3>Linhas de custos</h3>
-                        <h3>Linhas de custos</h3>
-                    </div>
-                    <hr />
-
-                    <div className="info-custos">
-                        <p className="name">(A) Rede Bancária - X Boletos</p>
-                        <p>R$0,XX</p>
-                        <p>R$XXX,XX</p>
-                    </div>
-                    <div className="info-custos">
-                        <p className="name">(B) Rede Sicoob - X Boletos</p>
-                        <p>R$0,XX</p>
-                        <p>R$XXX,XX</p>
-                    </div>
-                    <div className="info-custos">
-                        <p className="name">(C) Liquidação Própria Coop. - X Boletos</p>
-                        <p>R$0,XX</p>
-                        <p>R$XXX,XX</p>
-                    </div>
-                    <div className="info-custos">
-                        <p className="name">(D) Correspondente Bancário - X Boletos</p>
-                        <p>R$0,XX</p>
-                        <p>R$XXX,XX</p>
-                    </div>
-                    <div className="info-custos">
-                        <p className="name">(E) Tarifa Nova Cobrança - X Boletos</p>
-                        <p>R$0,XX</p>
-                        <p>R$XXX,XX</p>
-                    </div>
+                <div className="info-table">
+                    <h1>Custos Por Emissão</h1>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Linhas de Custos</th>
+                                <th>Custos Unitários</th>
+                                <th>Custos Totais</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className="table-first-column">(A) Rede Bancária - XXXX Boletos</td>
+                                <td>R$X,XX</td>
+                                <td>R$XXX,XX</td>
+                            </tr>
+                            <tr>
+                                <td className="table-first-column">(B) Rede Sicoob - XXX Boletos</td>
+                                <td>R$X,XX</td>
+                                <td>R$XXX,XX</td>
+                            </tr>
+                            <tr>
+                                <td className="table-first-column">(C) Correspondente Bancário - XXXX Boletos</td>
+                                <td>R$X,XX</td>
+                                <td>R$XXX,XX</td>
+                            </tr>
+                            <tr>
+                                <td className="table-first-column">(D) Processamento - XXXX Boletos</td>
+                                <td>R$X,XX</td>
+                                <td>R$XXX,XX</td>
+                            </tr>
+                            <tr>
+                                <td className="table-first-column">(E) Tarifa Nova Cobrança - XXXX Boletos</td>
+                                <td>R$X,XX</td>
+                                <td>R$XXX,XX</td>
+                            </tr>
+                            <tr>
+                                <td className="table-first-column">(=) Custos Totais:</td>
+                                <td>R$X,XX</td>
+                                <td>R$XXX,XX</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-                <hr />
-                <div className="ponto-de-equilibrio">
+                <div className="info-table">
                     <h1>Ponto de Equilíbrio</h1>
-
-                    <div className="info-custos">
-                        <p className="name">(+) Receitas Auferidas</p>
-                        <p>R$0,XX</p>
-                        <p>R$XXX,XX</p>
-                    </div>
-                    <div className="info-custos">
-                        <p className="name">(B) Rede Sicoob - X Boletos</p>
-                        <p>R$0,XX</p>
-                        <p>R$XXX,XX</p>
-                    </div>
-                    <div className="info-custos">
-                        <p className="name">(C) Liquidação Própria Coop. - X Boletos</p>
-                        <p>R$0,XX</p>
-                        <p>R$XXX,XX</p>
-                    </div>
-                    <div className="info-custos">
-                        <p className="name">(D) Correspondente Bancário - X Boletos</p>
-                        <p>R$0,XX</p>
-                        <p>R$XXX,XX</p>
-                    </div>
-                    <div className="info-custos">
-                        <p className="name">(E) Tarifa Nova Cobrança - X Boletos</p>
-                        <p>R$0,XX</p>
-                        <p>R$XXX,XX</p>
-                    </div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Por Boleto</th>
+                                <th>Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className="table-first-column">(+) Receitas Auferidas</td>
+                                <td>R$X,XX</td>
+                                <td>R$XXX,XX</td>
+                            </tr>
+                            <tr>
+                                <td className="table-first-column">(-) Custos Totais Por Boleto</td>
+                                <td>R$X,XX</td>
+                                <td>R$XXX,XX</td>
+                            </tr>
+                            <tr>
+                                <td className="table-first-column">(=) Falta/Excedente</td>
+                                <td>R$X,XX</td>
+                                <td>R$XXX,XX</td>
+                            </tr>
+                            <tr>
+                                <td className="table-first-column">Tarifa por baixa por decurso de prazo</td>
+                                <td><Input sx={{'--Input-radius': 0, '--Input-focusedInset': 'var(--any, )',}} value={formData.saldoMedio} className="input" onChange={(e) => handleInputChange('saldoMedio', e.target.value)} placeholder={'Insira aqui!'} variant="outlined" type="number" /></td>
+                                <td>R$XXX,XX</td>
+                            </tr>
+                            <tr>
+                                <td className="table-first-column">Tarifa na baixa pedido cedente</td>
+                                <td><Input sx={{'--Input-radius': 0}} value={formData.saldoMedio} className="input" onChange={(e) => handleInputChange('saldoMedio', e.target.value)} placeholder={'Insira aqui!'} variant="outlined" type="number" /></td>
+                                <td>R$XXX,XX</td>
+                            </tr>
+                            <tr>
+                                <td className="table-first-column">Tarifa na liquidação do título</td>
+                                <td><Input sx={{'--Input-radius': 0}} value={formData.saldoMedio} className="input" onChange={(e) => handleInputChange('saldoMedio', e.target.value)} placeholder={'Insira aqui!'} variant="outlined" type="number" /></td>
+                                <td>R$XXX,XX</td>
+                            </tr>
+                            <tr>
+                                <td className="table-first-column">Tarifa na entrada do título</td>
+                                <td><Input sx={{'--Input-radius': 0}} value={formData.saldoMedio} className="input" onChange={(e) => handleInputChange('saldoMedio', e.target.value)} placeholder={'Insira aqui!'} variant="outlined" type="number" /></td>
+                                <td>R$XXX,XX</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </main>
         </div>
