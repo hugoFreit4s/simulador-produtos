09edd6c11e051a5cd25a4fb43e41f832a7bd965a
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-07-19 13:49:23 -0300

    Novos estilos e novos displays adicionados na página Resultado Boleto

--- src/pages/ResultadoBoleto.js
+++ src/pages/ResultadoBoleto.js
@@ -1,39 +1,46 @@
-// ResultadoBoleto.js
 import React, { useRef } from 'react';
 import ReactToPrint from 'react-to-print';
 import DisplayInfo from "../components/DisplayInfo";
 import Printer from '../components/Printer';
 import DisableNumberScroll from '../DisableNumberScroll';
+import rBoletoStl from './ResultadoBoleto.module.css';
 
 const ResultadoBoleto = () => {
     DisableNumberScroll();
-
-    // Obter e converter valores do localStorage
     const qtdBoletos = parseFloat(localStorage.getItem('qtdBoletos')) || 0;
     const percentageTLiquidados = parseFloat(localStorage.getItem('percentageTLiquidados')) || 0;
     const ticketMedio = parseFloat(localStorage.getItem('ticketMedio')) || 0;
-
-    // Realizar cálculos
     const bLiquidados = (qtdBoletos * percentageTLiquidados) / 100;
     const saldoMedio = (bLiquidados * ticketMedio).toLocaleString('pt-BR', {
         style: "currency",
         currency: "BRL",
     });
-
-    // Referência para o conteúdo de impressão
     const componentRef = useRef();
 
+    // const handleCheckboxChange = (field1, field2, value) => {
+    //     setFormData((prevData) => ({
+    //         ...prevData,
+    //         [field1]: value,
+    //         [field2]: value ? false : prevData[field2],
+    //     }));
+    // };
+
     return (
-        <div className="ResultadoBoletos">
-            <DisplayInfo title={'Saldo Médio'} data={saldoMedio} />
+        <div className={rBoletoStl.ResultadoBoleto}>
+            <div className={rBoletoStl.section}>
+                <DisplayInfo title={'Saldo médio'} data={saldoMedio} />
+                <DisplayInfo title={'Porcentagem sobre saldo médio'} data={'???'} />
+                <DisplayInfo title={'Taxa CDI (a.m.)'} data={'???'} />
+                <DisplayInfo title={'Porcentagem centralização'} data={'???'} />
+            </div>
             <ReactToPrint
                 trigger={() => <button>Imprimir Saldo Médio</button>}
                 content={() => {
-                    console.log("Rendering for print:", componentRef.current); // Verifique o conteúdo
+                    console.log("Rendering for print:", componentRef.current);
                     return componentRef.current;
-                }}
-            />
-            <div style={{ color: 'black', display:'none' }}>
+                }} />
+
+            <div style={{ display: 'none' }}>
                 <Printer ref={componentRef} saldoMedio={saldoMedio} />
             </div>
         </div>
