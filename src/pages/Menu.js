4e25581d60edd06704347509ef445c51646202cf
Hugo de Freitas <hugo.encaminhados@gmail.com>
2024-07-19 12:10:51 -0300

    Menu responsivisado para todos os aparelhos

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,6 +1,6 @@
+import React, { useState } from "react";
 import Button from "../components/Button";
 import stl from "./Menu.module.css";
-import React, { useState } from "react";
 
 function Menu() {
     const [DropdownVisibility, setDropdownVisibility] = useState(false);
@@ -12,29 +12,34 @@ function Menu() {
     return (
         <div className={stl.main}>
             <div className={stl.dropdownmenu}>
-                <div className={stl.dropdownbox}>
-                    <label className={stl.dropdwonlabel} onClick={toggleDropdown}>Produtos</label>
-                    <ul className={`${stl.dropdown} ${!DropdownVisibility ? stl.hidden : ''}`}>
-                        <li>Boletos</li>
-                        <li>SIPAG</li>
-                    </ul>
+                <div className={stl.menuIcon} onClick={toggleDropdown}>
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </div>
+                <ul className={`${stl.dropdown} ${DropdownVisibility ? stl.visible : ''}`}>
+                    <li><a href="/boletos" className={stl.links}>Boletos</a></li>
+                    <li><a href="/" className={stl.links}>SIPAG</a></li>
+                    <li><a href="/" className={stl.links}>Produto X</a></li>
+                    <li><a href="/" className={stl.links}>Produto Y</a></li>
+                    <li><a href="/" className={stl.links}>Produto Z</a></li>
+                </ul>
             </div>
             <div className={stl.sidemenu}>
                 <div className={stl.sidemenutext}>
                     <a href="/boletos" className={stl.links}>
                         <p className={stl.sidemenup}>Boletos</p>
                     </a>
-                    <a href="/boletos" className={stl.links}>
+                    <a href="/" className={stl.links}>
                         <p className={stl.sidemenup}>SIPAG</p>
                     </a>
-                    <a href="/boletos" className={stl.links}>
+                    <a href="/" className={stl.links}>
                         <p className={stl.sidemenup}>Produto X</p>
                     </a>
-                    <a href="/boletos" className={stl.links}>
+                    <a href="/" className={stl.links}>
                         <p className={stl.sidemenup}>Produto Y</p>
                     </a>
-                    <a href="/boletos" className={stl.links}>
+                    <a href="/" className={stl.links}>
                         <p className={stl.sidemenup}>Produto Z</p>
                     </a>
                 </div>
@@ -50,20 +55,20 @@ function Menu() {
                         <h1>Fale Conosco</h1>
                         <p className={stl.outsidetext}>Sugestões, Dúvidas e Suporte: <br />Abra um fluxo direcionado a equipe de BI com o tema “Simulador de Produtos” na Intranet</p>
                     </div>
-                    <div>
+                    <div className={stl.btn}>
                         <a href="https://intranet.sicoobvaledoaco.com.br/login?r=Lw==" target="_blank">
                             <Button text={'INTRANET'} />
                         </a>
                     </div>
                 </div>
             </div>
-            <footer className={stl.logofooter}>
+            <footer>
                 <a href="/">
-                    <img className={stl.logo} src="logowhitevertical.png" alt='logo' style={{ height: '80px', width: '120px' }} />
+                    <img src="logowhitevertical.png" alt='logo' style={{ height: '80px', width: '120px' }} />
                 </a>
             </footer>
         </div>
-    )
+    );
 }
 
 export default Menu;
